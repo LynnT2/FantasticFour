5795d7288f4b0556360681c9659a23a782ed9667
Haiqi <77041179+yjwsb233@users.noreply.github.com>
2021-05-20 10:11:36 -1000

    final

--- Midterm/js/map.js
+++ Midterm/js/map.js
@@ -14,6 +14,7 @@ let csvdata;
 // initialize
 $( document ).ready(function() {
     createMap(lat,lon,zl);
+    getGeoJSON();
     readCSV(path1);
     readCSV(path2);
     createLayerControl();
@@ -104,3 +105,168 @@ function createLayerControl(){
     L.control.layers(null,toggle).addTo(map);
 }
 
+let path = '';
+
+// put this in your global variables
+let geojsonPath = 'data/us.geojson';
+let geojson_data;
+let geojson_layer;
+
+let brew = new classyBrew();
+let fieldtomap; 
+
+let legend = L.control({position: 'bottomright'});
+let info_panel = L.control();
+
+
+
+// function to get the geojson data
+function getGeoJSON(){
+
+	$.getJSON(geojsonPath,function(data){
+		console.log(data)
+
+		// put the data in a global variable
+		geojson_data = data;
+
+		// call the map function
+		mapGeoJSON('count') // add a field to be used
+	})
+}
+
+function mapGeoJSON(field){
+
+	// clear layers in case it has been mapped already
+	if (geojson_layer){
+		geojson_layer.clearLayers()
+	}
+	
+	// globalize the field to map
+	fieldtomap = field;
+
+	// create an empty array
+	let values = [];
+
+	// based on the provided field, enter each value into the array
+	geojson_data.features.forEach(function(item,index){
+		if(item.properties[field] != undefined){
+			values.push(item.properties[field])
+		}
+	})
+
+	// set up the "brew" options
+	brew.setSeries(values);
+	brew.setNumClasses(4);
+	brew.setColorCode('Reds');
+	brew.classify('quantiles');
+
+	// create the layer and add to map
+	geojson_layer = L.geoJson(geojson_data, {
+		style: getStyle, //call a function to style each feature
+		onEachFeature: onEachFeature //actions on eac feature
+	}).addTo(map);
+
+	map.fitBounds(geojson_layer.getBounds())
+
+	// create the legend
+	createLegend();
+	createInfoPanel();
+}
+
+function getStyle(feature){
+	return {
+		stroke: true,
+		color: 'white',
+		weight: 1,
+		fill: true,
+		fillColor: brew.getColorInRange(feature.properties[fieldtomap]),
+		fillOpacity: 0.8
+	}
+}
+
+function createLegend(){
+	legend.onAdd = function (map) {
+		var div = L.DomUtil.create('div', 'info legend'),
+		breaks = brew.getBreaks(),
+		labels = [],
+		from, to;
+		labels.push('Number of Hate Crimes')
+		
+		for (var i = 0; i < breaks.length; i++) {
+			from = breaks[i];
+			to = breaks[i + 1];
+			if(to) {
+				labels.push(
+					'<i style="background:' + brew.getColorInRange(to) + '"></i> ' +
+					from + ' &ndash; ' + to);
+				}
+			}
+			
+			div.innerHTML = labels.join('<br>');
+			return div;
+		};
+		
+		legend.addTo(map);
+}
+
+// Function that defines what will happen on user interactions with each feature
+function onEachFeature(feature, layer) {
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: zoomToFeature
+	});
+}
+
+// on mouse over, highlight the feature
+function highlightFeature(e) {
+	var layer = e.target;
+
+	// style to use on mouse over
+	layer.setStyle({
+		weight: 2,
+		color: '#666',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+		layer.bringToFront();
+	}
+
+	info_panel.update(layer.feature.properties)
+}
+
+// on mouse out, reset the style, otherwise, it will remain highlighted
+function resetHighlight(e) {
+	geojson_layer.resetStyle(e.target);
+	info_panel.update() // resets infopanel
+}
+
+// on mouse click on a feature, zoom in to it
+function zoomToFeature(e) {
+	map.fitBounds(e.target.getBounds());
+}
+
+function createInfoPanel(){
+
+	info_panel.onAdd = function (map) {
+		this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+		this.update();
+		return this._div;
+	};
+
+	// method that we will use to update the control based on feature properties passed
+	info_panel.update = function (properties) {
+		// if feature is highlighted
+		if(properties){
+			this._div.innerHTML = `<b>${properties.NAME}</b><br>${fieldtomap}: ${properties[fieldtomap]}`;
+		}
+		// if feature is not highlighted
+		else
+		{
+			this._div.innerHTML = 'Hover over a state to see the number of its anti-Asian hate incidents from March 2020 to March 2021.';
+		}
+	};
+
+	info_panel.addTo(map);
+}
